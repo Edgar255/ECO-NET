3b14f5a32b61f1fb8d9b94a03fa1ab518cbb2bd3
Edgar255 <149951879+Edgar255@users.noreply.github.com>
2024-07-18 09:32:00 +0200

    Initial commit

---
+++ script/pay.js
@@ -0,0 +1,5 @@
+document.querySelector('.payment-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Add your form validation or submission logic here
+    alert('Form submitted!');
+});
